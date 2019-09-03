4b98bc01e814cba280841797a40d46842432c85b
Tiago Ribeiro <tiago.ribeiro@inm.pt>
2019-09-03 15:43:23 +0100

    login view

--- application/src/main/kotlin/org/github/tarolas/travian/application/TravianView.kt
+++ application/src/main/kotlin/org/github/tarolas/travian/application/TravianView.kt
@@ -1,9 +1,22 @@
 package org.github.tarolas.travian.application
 
+import javafx.application.Platform
+import javafx.scene.control.Tab
 import javafx.scene.layout.BorderPane
 import org.github.tarolas.travian.application.gui.login.LoginFormView
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
 
 @Component
 class TravianView(val loginFormView: LoginFormView) : BorderPane() {
+
+    private val log = LoggerFactory.getLogger(TravianView::class.java)
+
+    init {
+        Platform.runLater {
+            log.info("Initializing  [{}] ", javaClass.simpleName)
+            center = loginFormView
+        }
+    }
+
 }
--- application/src/main/kotlin/org/github/tarolas/travian/application/gui/login/LoginFormView.kt
+++ application/src/main/kotlin/org/github/tarolas/travian/application/gui/login/LoginFormView.kt
@@ -2,89 +2,62 @@ package org.github.tarolas.travian.application.gui.login
 
 import javafx.application.Platform
 import javafx.geometry.Pos
-import javafx.scene.control.Button
-import javafx.scene.control.Label
-import javafx.scene.control.TextField
 import javafx.scene.layout.BorderPane
-import javafx.scene.layout.GridPane
-import javafx.scene.layout.VBox
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
-import org.controlsfx.glyphfont.FontAwesome.Glyph
 import org.github.tarolas.travian.application.gui.login.LoginFormConstants.TAB_NAME
-import org.github.tarolas.travian.application.utils.GlyphFactory
+import org.github.tarolas.travian.engine.TravianEngineBuilder
+import org.github.tarolas.travian.engine.entities.LoginParams
+import org.springframework.context.ApplicationContext
+import org.springframework.context.ApplicationContextAware
+import tornadofx.*
+import tornadofx.FX.Companion.messages
 
 @Component
-class LoginFormView(val model: LoginFormModel) : BorderPane() {
+class LoginFormView(val model: LoginFormModel) : BorderPane(), ApplicationContextAware {
+
+    private lateinit var springContext: ApplicationContext
+
+    override fun setApplicationContext(applicationContext: ApplicationContext) {
+        springContext = applicationContext
+    }
 
     private val log = LoggerFactory.getLogger(LoginFormView::class.java)
-
-    // GridPane
-    internal var gpMainGrid = GridPane()
-
-    // Labels
-    internal var lbUsername = Label("Username:")
-    internal var lbPassword = Label("Password:")
-    internal var lbServer = Label("Server:")
-
-    // Fields
-    internal var tfUserName = TextField()
-    internal var tfPassword = TextField()
-    internal var tfServer = TextField()
-
-    // Buttons
-    internal var btnSave = Button("LogIn")
-    internal var btnClose = Button("Close")
 
     init {
         log.debug("Initialized [{}]", javaClass.simpleName)
         Platform.runLater {
             userData = TAB_NAME
             layoutForm()
-            initFieldData()
-            bindFieldsToModel()
         }
     }
 
-    protected fun initFieldData() {
-        // Id and names are populated using bindings.
-        // Populate birth date and age category
-        btnSave.graphic = GlyphFactory.create(Glyph.SAVE)
-        btnClose.graphic = GlyphFactory.create(Glyph.CLOSE)
-    }
-
     protected fun layoutForm() {
-        gpMainGrid.alignment = Pos.CENTER
-
-        gpMainGrid.hgap = 5.0
-        gpMainGrid.vgap = 5.0
-
-        gpMainGrid.add(lbUsername, 1, 1)
-        gpMainGrid.add(lbPassword, 1, 2)
-        gpMainGrid.add(lbServer, 1, 3)
-
-        gpMainGrid.add(tfUserName, 2, 1)
-        gpMainGrid.add(tfPassword, 2, 2)
-        gpMainGrid.add(tfServer, 2, 3)
-
-        // Add buttons and make them the same width
-        val buttonBox = VBox(btnSave, btnClose)
-        buttonBox.spacing = 2.0
-        btnSave.maxWidth = java.lang.Double.MAX_VALUE
-        btnClose.maxWidth = java.lang.Double.MAX_VALUE
-
-        // TitledBorderedPane titledPane = Borders.wrapWithTitledBorderedPane("Edit",
-        // null, buttonBox) titlepane example
-        gpMainGrid.add(buttonBox, 3, 1, 1, 5)
-
-        // Set the prompt text for the birth date field
-        center = gpMainGrid
+        center = form {
+            alignment = Pos.CENTER
+            fieldset(messages["login.title"]) {
+                vbox {
+                    field(messages["username.label"]) {
+                        textfield().bind(model.username)
+                    }
+                    field(messages["password.label"]) {
+                        passwordfield().bind(model.password)
+                    }
+                    field(messages["server.label"]) {
+                        textfield().bind(model.server)
+                    }
+                    button(messages["button.label"]) {
+//                        graphic = GlyphFactory.create(Glyph.SAVE)
+                        action {
+                            log.info("logIn: username:${model.username.get()}, password:${model.password.get()}, server: ${model.server.get()}")
+                            val engine  = TravianEngineBuilder.getEngine(springContext, LoginParams(model.username.get(), model.password.get(), model.server.get()))
+                        }
+                        // Save button is disabled until every field has a value
+                        disableProperty().bind(model.username.isNull.or(model.password.isNull
+                                .or(model.server.isNull)))
+                    }
+                }
+            }
+        }
     }
-
-    protected fun bindFieldsToModel() {
-        tfUserName.textProperty().bind(model.username)
-        tfPassword.textProperty().bindBidirectional(model.password)
-        tfServer.textProperty().bindBidirectional(model.server)
-    }
-
 }
