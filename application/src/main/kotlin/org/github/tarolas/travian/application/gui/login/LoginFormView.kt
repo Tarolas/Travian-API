ea3fed2fe89c62956ce2edc0c7ba0ae3299806bc
Tiago Ribeiro <tiagodribeiro@hotmail.com>
2019-09-11 19:10:12 +0100

    create aux suspension function

--- application/src/main/kotlin/org/github/tarolas/travian/application/gui/login/LoginFormView.kt
+++ application/src/main/kotlin/org/github/tarolas/travian/application/gui/login/LoginFormView.kt
@@ -1,7 +1,7 @@
 package org.github.tarolas.travian.application.gui.login
 
 import javafx.geometry.Pos
-import kotlinx.coroutines.runBlocking
+import org.github.tarolas.travian.application.utils.runAsyncSuspend
 import org.springframework.stereotype.Component
 import tornadofx.*
 import java.util.*
@@ -41,7 +41,7 @@ class LoginFormView(
                         //                        graphic = GlyphFactory.create(Glyph.SAVE)
                         action {
                             log.info("logIn: username:${model.username.get()}, password:${model.password.get()}, server: ${model.server.get()}")
-                            runAsync { runBlocking { controller.login(model) } }
+                            runAsyncSuspend { controller.login(model) }
                         }
                         // Save button is disabled until every field has a value
                         disableProperty().bind(model.username.isNull.or(model.password.isNull
@@ -51,33 +51,6 @@ class LoginFormView(
             }
         }
     }
-
-//    protected fun layoutForm() {
-//        center = form {
-//            alignment = Pos.CENTER
-//            fieldset(messages["login.title"]) {
-//                vbox {
-//                    field(messages["username.label"]) {
-//                        textfield().bind(model.username)
-//                    }
-//                    field(messages["password.label"]) {
-//                        passwordfield().bind(model.password)
-//                    }
-//                    field(messages["server.label"]) {
-//                        textfield().bind(model.server)
-//                    }
-//                    button(messages["button.label"]) {
-////                        graphic = GlyphFactory.create(Glyph.SAVE)
-//                        action {
-//                            log.info("logIn: username:${model.username.get()}, password:${model.password.get()}, server: ${model.server.get()}")
-//                            controller.login(model.username.get(), model.password.get(), model.server.get())
-//                        }
-//                        // Save button is disabled until every field has a value
-//                        disableProperty().bind(model.username.isNull.or(model.password.isNull
-//                                .or(model.server.isNull)))
-//                    }
-//                }
-//            }
-//        }
-//    }
 }
+
+
--- application/src/main/kotlin/org/github/tarolas/travian/application/utils/Utils.kt
+++ application/src/main/kotlin/org/github/tarolas/travian/application/utils/Utils.kt
@@ -1,10 +1,22 @@
 package org.github.tarolas.travian.application.utils
 
+import kotlinx.coroutines.*
 import org.springframework.beans.factory.BeanFactory
 import org.springframework.context.ApplicationContext
+import reactor.core.publisher.toMono
+import tornadofx.*
 
 /**
  * @author tiago.ribeiro
  */
 
 inline fun <reified T> ApplicationContext.getBean() = this.getBean(T::class.java)
+
+fun <T> Component.runAsyncSuspend(status: TaskStatus? = find(scope), func: suspend FXTask<*>.() -> T) = runAsync(status) {
+    runBlocking(Dispatchers.Main) {
+        this@runAsync.func()
+    }
+}
+
+
+//fun <T> runAsync(status: TaskStatus? = find(scope), func: FXTask<*>.() -> T) = task(status, func)
